96ae73bd1f72944e283abff28102107e3613821c
Qing Wu <wiserfirst@gmail.com>
2019-08-02 22:10:58 +1000

    handle invalid argument

--- lib/leapyear.ex
+++ lib/leapyear.ex
@@ -3,10 +3,17 @@ defmodule Leapyear do
   Documentation for Leapyear.
   """
 
+  require Logger
+
   @doc "determine if a year is leap year or not"
   @spec leapyear?(integer()) :: boolean()
-  def leapyear?(year) do
+  def leapyear?(year) when is_integer(year) do
     (rem(year, 4) == 0 and rem(year, 100) != 0) or
       rem(year, 400) == 0
+  end
+
+  def leapyear?(year) do
+    Logger.warn("Invalid year: #{inspect(year)}")
+    false
   end
 end
